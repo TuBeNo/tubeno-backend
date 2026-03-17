31757bef35867f779d60ca0e416dc51d20772a87
TuBeNo <rolf.aune@gmail.com>
2026-03-17 13:33:29 +0100

    Create pkce.js

---
+++ api/pkce.js
@@ -0,0 +1,16 @@
+
+export function generateCodeVerifier() {
+  const array = new Uint32Array(56);
+  crypto.getRandomValues(array);
+  return Array.from(array, dec => ('0' + dec.toString(16)).substr(-2)).join('');
+}
+
+export async function generateCodeChallenge(verifier) {
+  const data = new TextEncoder().encode(verifier);
+  const digest = await crypto.subtle.digest("SHA-256", data);
+  const hash = btoa(String.fromCharCode(...new Uint8Array(digest)))
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+  return hash;
+}
