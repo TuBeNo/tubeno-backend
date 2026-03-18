bed6bdeeb26a35efe4c1bae305dc3b23d70af944
TuBeNo <rolf.aune@gmail.com>
2026-03-18 10:18:35 +0100

    Delete callback.js

--- callback.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { code, state, error } = req.query;
-
-  if (error) {
-    return res.status(400).send("Spotify login feilet: " + error);
-  }
-
-  return res.send("Innlogging mottatt! Du kan gå tilbake til TuBeNo-enheten.");
-}
